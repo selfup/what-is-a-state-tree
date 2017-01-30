7a9a7ac03b3c964e87601dca7bba9dcea2c169a9
selfup <boudinot.regis@yahoo.com>
2017-01-30 00:51:54 -0700

    expected end product of lesson

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
 const app = document.querySelector('#app');
 
+class Store {
+  constructor(state = {}, actions = {}, render = () => {}) {
+    this.state = state;
+    this.actions = actions;
+    this.render = render;
+    this.dispatch = (action, options) => {
+      const newValue = this.actions[action](this.state, options);
+      Object.assign(this.state, newValue);
+      this.render(this);
+    };
+  }
+}
+
+const state = {
+  message: new Date().getTime(),
+};
+
+const actions = {
+  MODIFY() {
+    return { message: new Date().getTime() };
+  },
+};
+
+const render = (store) => {
+  app.innerHTML = template(store);
+};
+
+const store = new Store(state, actions, render);
+
 const template = ({ state }) => {
   const { message } = state;
 
   return (`
     <div>
       <h1 id="header-tag"> ${message} </h1>
+      <button
+        onclick='store.dispatch("MODIFY")'
+      >
+        Change Number
+      </button>
     </div>
   `);
 };
 
-const render = (store) => {
-  if (!store || !store.state) return null;
-  app.innerHTML = template(store);
-};
-
-/**
-  Until we build our own Store Object
-  keep render(store) commented out until we copy paste it in!
-*/
-
-// render(store);
+render(store);
