b0a594f601c63a36f199e9b8c22955276ecd1be5
selfup <boudinot.regis@yahoo.com>
2017-02-22 23:51:49 -0700

    closures are so awesome

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-const app = document.querySelector('#app');
-
 class Store {
-  constructor(state = {}, actions = {}, render = () => {}) {
+  constructor(state = {}, actions = {}, paint = () => {}) {
     this.state = state;
     this.actions = actions;
-    this.render = render;
-    this.dispatch = (action, options) => {
-      const newValue = this.actions[action](this.state, options);
-      Object.assign(this.state, newValue);
-      this.render(this);
-    };
+    this.paint = paint;
+    this.dispatch = this.dispatch.bind(this);
   }
+
+  dispatch(action, options) {
+    const newValue = this.actions[action](this.state, options);
+    Object.assign(this.state, newValue);
+    this.paint(this), 'app';
+  };
 }
+
+const render = (template, id) => {
+  const app = document.getElementById(id);
+
+  return () => {
+    app.innerHTML = template(store);
+  };
+};
+
+// END OF API
 
 const state = {
   message: new Date().getTime(),
@@ -19,24 +29,20 @@ const state = {
 
 const actions = {
   MODIFY() {
-    return { message: new Date().getTime() };
+    return {
+      message: new Date().getTime(),
+    };
   },
 };
 
-const render = (store) => {
-  app.innerHTML = template(store);
-};
-
-const store = new Store(state, actions, render);
-
-const template = ({ state }) => {
+const App = ({ state }) => {
   const { message } = state;
 
   return (`
     <div>
       <h1 id="header-tag"> ${message} </h1>
       <button
-        onclick='store.dispatch("MODIFY")'
+        onclick='dispatch("MODIFY")'
       >
         Change Number
       </button>
@@ -44,4 +50,8 @@ const template = ({ state }) => {
   `);
 };
 
-render(store);
+const store = new Store(state, actions, render(App, 'app'));
+
+const dispatch = store.dispatch;
+
+render(App, 'app')(store);
